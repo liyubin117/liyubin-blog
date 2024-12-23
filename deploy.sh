22a283e3bc0a726f6d016452c61daedc0722b84d
Yubin Li <lixin58688@163.com>
2024-12-23 10:46:57 +0800

    enhance deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
-set -e
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd build
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:messiahhh/messiahhh.github.io.git master
-
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:messiahhh/blog.git master:gh-pages
-#git push -f https://${access_token}github.com/liyubin117/liyubin-blog.git HEAD:gh-pages
- git push -f git@github.com:liyubin117/liyubin117.github.io.git HEAD:gh-pages
-
-cd -
+npm run clear
+npm install
+npm run build -- --no-minify
+USE_SSH=true npm run deploy -- --skip-build
